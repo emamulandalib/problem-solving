d68000b232ef0722356e1e0e40d338f71f8e397c
Emamul Andalib <emamul.andalib@gmail.com>
2022-03-01 13:56:40 +0000

    Rotate array solved

---
+++ leetcode/dotnet/Program.cs
@@ -0,0 +1,2 @@
+﻿var rotateArray = new RotateArray.Solution();
+rotateArray.Rotate(new int[] { 1, 2, 3, 4 }, 3);
---
+++ leetcode/dotnet/rotate_array.cs
@@ -0,0 +1,22 @@
+namespace RotateArray;
+
+public class Solution
+{
+    public void Rotate(int[] nums, int k)
+    {
+        var n = nums.Length;
+        k = k % n;
+        Reverse(nums, 0, n- 1);
+        Reverse(nums, 0, k - 1);
+        Reverse(nums, k, n - 1);
+    }
+
+    private void Reverse(int[] nums, int l, int r)
+    {
+        while (l <= r)
+        {
+            (nums[l], nums[r]) = (nums[r], nums[l]);
+            (l, r) = (l + 1 , r - 1);
+        }
+    }
+}
